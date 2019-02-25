e1d8374e49f59c4ecb6eabdeb30e12e13fed07c8
Thomas Voggenreiter <t.voggenreiter01@gmail.com>
2019-02-25 09:40:48 +0100

    undo changes to tl_content

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -9,8 +9,6 @@
  * file that was distributed with this source code.
  */
 
-namespace ContaoSwiperBundle\Resources\contao\dca;
-
 $GLOBALS['TL_DCA']['tl_content']['palettes']['swiperStart'] = '{type_legend},type;{slider_legend},sliderDelay,sliderSpeed,sliderSlidesPerView,sliderSpaceBetween,sliderEffect,sliderWrapperClass,sliderContinuous,sliderButtons,sliderPagination,sliderPaginationType,sliderBreakpoints;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['swiperStop'] = $GLOBALS['TL_DCA']['tl_content']['palettes']['sliderStop'];
 
