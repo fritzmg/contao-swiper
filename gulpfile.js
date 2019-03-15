d25f2b0bd9a18672a48c792e74854ae6bc47cd21
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-03-15 12:01:48 +0100

    add gulp task

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+gulp.task('compressJS', function(){
+  return gulp.src(['src/Resources/public/contao-swiper.js'])
+             .pipe(plumber())
+             .pipe(rename({ suffix: '.min' }))
+             .pipe(uglify())
+             .pipe(gulp.dest('src/Resources/public/'));
+});
+
+// Watchers
+gulp.task('watch', function(callback) {
+  runSequence([
+      'compressJS'
+    ], [
+      'watch:js',
+    ],
+    callback
+  );
+});
+
+gulp.task('watch:js', function() {
+  gulp.watch('files/themes/**/*.js', ['compressJS']);
+});
+
+gulp.task('default', function(callback) {
+  runSequence(['watch'],
+    callback
+  );
+});
+
+gulp.task('build', function(callback) {
+  runSequence([
+      'compressJS'
+    ],
+    callback
+  );
+});
